ba771fee93a62bab681ec512e216f85d03f5df34
ISO0120012 Полтавский Даниил Сергеевич <ISO0120012@sibupk.su>
2024-11-16 13:55:32 +0700

    Laba 3

---
+++ Laba 3/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = this.tbName.Text.Trim();
+            if (username.Length >= 3)
+            {
+                MessageBox.Show("Привет!" + username);
+            }
+            else
+            {
+                MessageBox.Show("Введите именя пользователя");
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
